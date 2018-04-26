38302deac1441118dcee5145bfb66aa4e4c8a5b1
Minh Hieu <hieunm1987@gmail.com>
2018-04-26 16:45:13 +0700

    change table name

--- chatApp.sql
+++ chatApp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2018 at 05:08 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Apr 26, 2018 at 11:43 AM
+-- Server version: 10.1.22-MariaDB
+-- PHP Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,10 +27,10 @@ DELIMITER $$
 -- Procedures
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `createConversion` (IN `user_id` INT(11), IN `friendid` INT(11))  NO SQL
-INSERT INTO `conversions_list`(`user1_id`, `user2_id`) 
+INSERT INTO `conversion`(`user1_id`, `user2_id`) 
 SELECT * FROM (SELECT user_id, friendid) AS tmp
 WHERE NOT EXISTS (
-    SELECT * FROM conversions_list WHERE (user1_id = user_id AND user2_id=friendid) or (user1_id = friendid AND user2_id=user1_id)
+    SELECT * FROM `conversion` WHERE (user1_id = user_id AND user2_id=friendid) or (user1_id = friendid AND user2_id=user1_id)
 ) LIMIT 1$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `displayMessage` (IN `user_id` INT(11), IN `friend_id` INT(11))  NO SQL
@@ -46,28 +46,28 @@ SELECT
     	WHEN differ=0 THEN date_format(time, '%H:%i') 
         WHEN differ=1 THEN date_format(time, 'Hôm qua, %H:%i')
         WHEN differ < 7 and differ > 1 THEN CONCAT('Thứ ', DAYOFWEEK(time), date_format(time, ', %H:%i'))
-            ELSE time
+                ELSE time
 	END as time,
     message_color
 FROM
     (
     SELECT
-        c.id,
-        c.conversion_id,
-        c.message_content,
-        c.sender_id,
-        c.time,
+        m.id,
+        m.conversion_id,
+        m.message_content,
+        m.sender_id,
+        m.time,
         DATEDIFF(NOW(), c.time) differ,
         u.name,
         u.alias,
-        l.message_color
+        c.message_color
     FROM
+        `message` m,
         `conversion` c,
-        `conversions_list` l,
-        users u
+        `users` u
     WHERE
-        c.conversion_id = l.id AND 
-        c.sender_id = u.id AND `conversion_id` IN(
+        m.conversion_id = c.id AND 
+        m.sender_id = u.id AND `conversion_id` IN(
         SELECT
             id
         FROM
@@ -79,13 +79,13 @@ ORDER BY
     c.time) AS CONV$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertMessage` (IN `user_id` INT(11), IN `message` VARCHAR(20000) CHARSET utf8, IN `friendid` INT(11))  NO SQL
-INSERT INTO `conversion`(`conversion_id`, `message_content`, `sender_id`, `time`) 
-    SELECT `id`, message, user_id, now() FROM `conversions_list` WHERE (`user1_id`=user_id AND `user2_id`=friendid) or (`user1_id`=friendid AND `user2_id`=user_id)$$
+INSERT INTO `message`(`conversion_id`, `message_content`, `sender_id`, `time`) 
+    SELECT `id`, message, user_id, now() FROM `conversion` WHERE (`user1_id`=user_id AND `user2_id`=friendid) or (`user1_id`=friendid AND `user2_id`=user_id)$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertUser` (IN `usr` VARCHAR(255) CHARSET utf8, IN `pwd` VARCHAR(50), IN `eml` VARCHAR(255) CHARSET utf8, IN `ali` VARCHAR(255) CHARSET utf8, IN `pho` VARCHAR(15))  NO SQL
 INSERT INTO `users`(`name`, `password`, `email`, `alias`, `phone`) VALUES (usr, pwd, eml, ali, pho)$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `searchUsers` (IN `uid` INT(11))  NO SQL
+CREATE DEFINER=`root`@`localhost` PROCEDURE `searchUsers` (IN `uid` INT(11), IN `txt` TEXT)  NO SQL
 SELECT * FROM 
 (
     SELECT
@@ -95,13 +95,13 @@ SELECT * FROM
     	u.`email`,
         u.`alias`
     FROM
-        conversion c,
-        conversions_list l,
-        users u
+        `message` m,
+        `conversion` c,
+        `users` u
     WHERE
-        c.`conversion_id` = l.`id`
+        m.`conversion_id` = c.`id`
         AND u.id = `user1_id` + `user2_id` - uid
-        AND (l.`user1_id` = uid OR l.`user2_id` = uid)
+        AND (c.`user1_id` = uid OR c.`user2_id` = uid)
         AND (
             u.`name` like concat('%',txt,'%')
             or u.`alias` like concat('%',txt,'%')
@@ -109,7 +109,7 @@ SELECT * FROM
             or u.`phone`=txt
         )
     GROUP BY user_id
-    ORDER BY MAX(c.`time`) DESC
+    ORDER BY MAX(m.`time`) DESC
 ) as t1
 UNION
 (
@@ -124,13 +124,13 @@ where u.`id` not in (
     SELECT
         `user1_id` + `user2_id` - uid
     FROM
-        conversion c,
-        conversions_list l,
-        users u
+        `message` m,
+        `conversion` c,
+        `users` u
     WHERE
-        c.`conversion_id` = l.`id`
+        m.`conversion_id` = c.`id`
         AND u.id = `user1_id` + `user2_id` - uid
-        AND (l.`user1_id` = uid OR l.`user2_id` = uid)
+        AND (c.`user1_id` = uid OR c.`user2_id` = uid)
     )
     and u.`id` != uid
     AND (
@@ -151,6 +151,37 @@ DELIMITER ;
 
 CREATE TABLE `conversion` (
   `id` int(11) NOT NULL,
+  `user1_id` int(11) DEFAULT NULL,
+  `user2_id` int(11) DEFAULT NULL,
+  `message_color` int(11) DEFAULT '34047'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `conversion`
+--
+
+INSERT INTO `conversion` (`id`, `user1_id`, `user2_id`, `message_color`) VALUES
+(25, 1, 2, 6797416),
+(31, 1, 8, 34047),
+(32, 1, 6, 34047),
+(33, 1, 3, 7751423),
+(34, 1, 5, 4505287),
+(35, 9, 1, 34047),
+(37, 1, 17, 34047),
+(38, 1, 11, 34047),
+(39, 1, 10, 34047),
+(40, 1, 29, 34047),
+(41, 1, 12, 34047),
+(42, 6, 2, 34047);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
   `conversion_id` int(11) DEFAULT NULL,
   `message_content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `sender_id` int(11) DEFAULT NULL,
@@ -158,17 +189,15 @@ CREATE TABLE `conversion` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `conversion`
+-- Dumping data for table `message`
 --
 
-INSERT INTO `conversion` (`id`, `conversion_id`, `message_content`, `sender_id`, `time`) VALUES
-(14, 25, 'me may', 1, '2018-04-16 15:31:54'),
-(15, 25, 'toi a ve muon', 1, '2018-04-17 16:43:13'),
+INSERT INTO `message` (`id`, `conversion_id`, `message_content`, `sender_id`, `time`) VALUES
+(15, 25, 'toi a ve muon', 1, '2018-04-16 16:43:13'),
 (16, 25, 'danh aoe', 1, '2018-04-17 16:43:15'),
 (17, 25, 'okkkkkkkkk', 2, '2018-04-17 16:43:29'),
 (20, 25, 'nho ve som', 2, '2018-04-17 16:47:18'),
 (23, 25, 'okkkkk', 1, '2018-04-17 16:48:10'),
-(26, 25, 'me may`', 1, '2018-04-18 08:59:49'),
 (27, 25, 'cho lac di vien chua', 1, '2018-04-18 08:59:52'),
 (28, 31, 'chu tach cmnr =))', 1, '2018-04-18 09:00:31'),
 (29, 32, 'toi co ve lo duc khong', 1, '2018-04-18 09:00:40'),
@@ -189,15 +218,12 @@ INSERT INTO `conversion` (`id`, `conversion_id`, `message_content`, `sender_id`,
 (58, 25, 'Theo quy định của hợp đồng, Bảo hiểm đồng ý thanh toán những chi phí khám và thuốc hợp lý, cần thiết về mặt y tế, theo chỉ định của bác sĩ điều trị, phát sinh khi người được bảo hiểm phải điều trị bệnh mà việc điều trị được bảo hiểm, không thanh toán chi phí mua mỹ phẩm, thực phẩm chức năng, vật tư y tế. Do đó rất tiếc bảo hiểm từ chối thanh toán các chi phí sau vì không thuộc phạm vi bảo hiểm: - Gạc phãu thuật bằng 7.800 vnd - Mỹ phẩm \'\' Avene - Ciacalfate \'\' bằng 409.882 vnd đồng thời sản phẩm được kê trên phiếu tư vấn, không phải đơn thuốc.', 1, '2018-04-18 16:53:56'),
 (59, 25, '-          Chính sách trẻ em tại khách sạn: Miễn phí cho 2 trẻ em dưới 12 tuổi ngủ chung phòng.  Ăn sáng phụ thu theo chiều cao. Dưới 1m miễn phí. Từ 1m đến 1.2m tính 60k. Từ 1.3m trở lên tính 120k  -          Extra bed: 300k/giường.  -          Công ty chỉ tài trợ chi phí Gala Dinner cho các FISer và người thân là Vợ/chồng và con. Trong TH là CB đưa bố mẹ, anh chị em đi nghỉ mát cùng, vui lòng confirm lại giúp em về số lượng tham gia. BTC sẽ tính phí tương đương 500k/người.', 1, '2018-04-18 17:02:40'),
 (60, 25, 'abc', 1, '2018-04-19 08:34:45'),
-(61, 25, '123', 2, '2018-04-19 08:35:33'),
-(62, 25, '456', 1, '2018-04-19 08:35:53'),
-(63, 36, 'uong nuoc de<br />', 2, '2018-04-19 09:12:27'),
-(64, 37, 'uong nuoc de<br />', 1, '2018-04-19 09:13:04'),
-(65, 38, 'dao nay lam an the nao roi<br />', 1, '2018-04-19 10:17:06'),
-(66, 25, 'me may`<br />', 1, '2018-04-19 10:39:01'),
+(61, 25, '???', 2, '2018-04-19 08:35:33'),
+(62, 25, ':3', 1, '2018-04-19 08:35:53'),
+(64, 37, 'uong nuoc de', 1, '2018-04-19 09:13:04'),
+(65, 38, 'dao nay lam an the nao roi', 1, '2018-04-19 10:17:06'),
 (67, 31, 'chan vai dai', 1, '2018-04-19 10:55:33'),
 (68, 32, 'a', 1, '2018-04-19 10:56:41'),
-(69, 25, 'me may`', 1, '2018-04-19 11:00:05'),
 (70, 34, 'lam an the nao roi', 1, '2018-04-19 11:08:52'),
 (71, 37, 'toi di duoc khong', 1, '2018-04-19 11:12:11'),
 (72, 33, 'lanh qua\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'\'', 1, '2018-04-19 11:36:17'),
@@ -205,41 +231,29 @@ INSERT INTO `conversion` (`id`, `conversion_id`, `message_content`, `sender_id`,
 (74, 39, 'co tien chua', 1, '2018-04-19 11:43:48'),
 (75, 40, 'dao nay lam an the nao roi`', 1, '2018-04-19 11:44:00'),
 (76, 41, 'trung bua', 1, '2018-04-19 11:45:32'),
-(77, 31, 'e biet ma', 8, '2018-04-19 21:36:50'),
-(78, 31, 'khong bat ngo', 8, '2018-04-19 21:36:53'),
-(79, 42, 'NGU!!!!!!!', 9, '2018-04-19 22:04:30');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `conversions_list`
---
-
-CREATE TABLE `conversions_list` (
-  `id` int(11) NOT NULL,
-  `user1_id` int(11) DEFAULT NULL,
-  `user2_id` int(11) DEFAULT NULL,
-  `message_color` int(11) DEFAULT 34047
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `conversions_list`
---
-
-INSERT INTO `conversions_list` (`id`, `user1_id`, `user2_id`) VALUES
-(25, 1, 2),
-(31, 1, 8),
-(32, 1, 6),
-(33, 1, 3),
-(34, 1, 5),
-(35, 9, 1),
-(36, 2, 17),
-(37, 1, 17),
-(38, 1, 11),
-(39, 1, 10),
-(40, 1, 29),
-(41, 1, 12),
-(42, 9, 3);
+(77, 25, 'mẹ mày', 1, '2018-04-19 15:01:42'),
+(78, 31, 'vang e cung du doan tu truoc roi`', 8, '2018-04-20 09:08:28'),
+(79, 31, '=))', 8, '2018-04-20 09:09:41'),
+(80, 25, 'lac tiem chua', 1, '2018-04-20 09:24:39'),
+(81, 25, 'tiem roi', 2, '2018-04-20 09:24:54'),
+(82, 25, 'dang ngu nhe', 2, '2018-04-20 09:24:56'),
+(83, 25, 'okkkkkkkkkkkkkkkk', 1, '2018-04-20 09:31:40'),
+(84, 25, 'mẹ mày mang ipad vào viện à', 1, '2018-04-20 14:02:30'),
+(85, 25, 'lac dang kham them o phong kham ngoai nhe', 2, '2018-04-20 16:51:02'),
+(86, 25, 'okkkkkkkkkkk', 2, '2018-04-20 16:57:19'),
+(87, 25, 'ke cai me', 1, '2018-04-20 16:58:09'),
+(88, 32, 'co\'', 6, '2018-04-20 17:08:46'),
+(89, 42, 'lac hom nay di tiem co khoc khong', 6, '2018-04-20 17:09:53'),
+(90, 42, 'khong', 2, '2018-04-20 17:10:08'),
+(91, 42, 'hom nay chi cho no di kham ngoai`', 2, '2018-04-20 17:10:13'),
+(92, 25, 'xin chuyen vien chua', 1, '2018-04-23 09:06:18'),
+(93, 25, 'đang bảo nó rồi', 2, '2018-04-23 09:14:04'),
+(94, 25, 'okkkkkkkkkkkkkkkkkkkkkk', 1, '2018-04-23 09:14:20'),
+(95, 25, 'lllllll', 2, '2018-04-23 09:14:27'),
+(96, 25, '?????', 1, '2018-04-23 09:17:58'),
+(97, 25, 'me may`', 1, '2018-04-23 10:57:05'),
+(98, 25, 'xin chuyen vien chua', 1, '2018-04-23 10:57:13'),
+(99, 25, 've nha roi`', 2, '2018-04-26 09:56:17');
 
 -- --------------------------------------------------------
 
@@ -282,8 +296,8 @@ INSERT INTO `users` (`id`, `name`, `password`, `email`, `alias`, `phone`, `statu
 (32, 'namcvi', 'c4ca4238a0b923820dcc509a6f75849b', NULL, 'Nam Cung', '0934435050', 1),
 (34, 'sutet1992', 'c4ca4238a0b923820dcc509a6f75849b', NULL, 'Kim Anh Bé', NULL, 1),
 (42, 'minhtuyen.ngo.96', 'c4ca4238a0b923820dcc509a6f75849b', NULL, 'Tuyen Ngo', NULL, 1),
-(43, 'hanh.su.9212', 'c4ca4238a0b923820dcc509a6f75849b', '', 'Hanh su', '0902557728', 1),
-(44, 'hakhaccuong', 'c4ca4238a0b923820dcc509a6f75849b', '', 'Cường Khắc Hà', '0985090099', 1);
+(43, 'hanh.su.9212', 'c4ca4238a0b923820dcc509a6f75849b', NULL, 'Hanh su', '0902557728', 1),
+(44, 'hakhaccuong', 'c4ca4238a0b923820dcc509a6f75849b', NULL, 'Cường Khắc Hà', '0985090099', 1);
 
 --
 -- Indexes for dumped tables
@@ -294,15 +308,16 @@ INSERT INTO `users` (`id`, `name`, `password`, `email`, `alias`, `phone`, `statu
 --
 ALTER TABLE `conversion`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `sender_id` (`sender_id`);
-
---
--- Indexes for table `conversions_list`
---
-ALTER TABLE `conversions_list`
-  ADD PRIMARY KEY (`id`),
   ADD KEY `user1_id` (`user1_id`),
   ADD KEY `user2_id` (`user2_id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sender_id` (`sender_id`),
+  ADD KEY `conversion_ibfk_2` (`conversion_id`);
 
 --
 -- Indexes for table `users`
@@ -318,20 +333,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `conversion`
 --
 ALTER TABLE `conversion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
-
---
--- AUTO_INCREMENT for table `conversions_list`
---
-ALTER TABLE `conversions_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
-
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-
 --
 -- Constraints for dumped tables
 --
@@ -340,20 +352,15 @@ ALTER TABLE `users`
 -- Constraints for table `conversion`
 --
 ALTER TABLE `conversion`
-  ADD CONSTRAINT `conversion_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `conversion_ibfk_1` FOREIGN KEY (`user1_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `conversion_ibfk_2` FOREIGN KEY (`user2_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `conversion`
+-- Constraints for table `message`
 --
-ALTER TABLE `conversion`
-  ADD CONSTRAINT `conversion_ibfk_2` FOREIGN KEY (`conversion_id`) REFERENCES `conversions_list` (`id`);
-
---
--- Constraints for table `conversions_list`
---
-ALTER TABLE `conversions_list`
-  ADD CONSTRAINT `conversions_list_ibfk_1` FOREIGN KEY (`user1_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `conversions_list_ibfk_2` FOREIGN KEY (`user2_id`) REFERENCES `users` (`id`);
+ALTER TABLE `message`
+  ADD CONSTRAINT `message_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `message_ibfk_2` FOREIGN KEY (`conversion_id`) REFERENCES `conversion` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
