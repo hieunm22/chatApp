6c598df2f14f328123bdd190d04fa48be1ef0ea9
Ming Hieu <hieunm1987@gmail.com>
2018-05-31 23:29:36 +0700

    avatar resources

--- controller/index_search.php
+++ controller/index_search.php
@@ -25,13 +25,18 @@
         }
         while ($row = mysqli_fetch_array($query)) {
             $msg = $row['message_content'];
-            if (strlen($msg) > 35) $msg = substr($msg, 0, 35).'...';
+            if (strlen($msg) > 30) $msg = substr($msg, 0, 30).'...';
             // chỉ bôi đậm khi message status là đã nhận và người gửi cuối không phải là mình
             $isunread = $row["msgstatus"] == 2 && $id != $row["last_sender_id"];
             if ($isunread) $unreadCount++;
             $txt_unread = $isunread ? ' unread-txt' : '';
+			$avatar = '';
+			if ($row['avatar_url'] == null) 
+				$avatar = $row["gender"] == 1 ? 'images\\\\2Q==.jpg' : 'images\\\\9k=.jpg';
+			else
+				$avatar = $row["avatar_url"];
             $html .= '<div class="lbl search-result" draggable="true">
-		<div class="avatar-img"><img src="images\\\\'.($row["gender"] == 1 ? '2Q==.jpg' : '9k=.jpg').'" style="width: 50px; height: 50px;"></div>
+		<div class="avatar-img"><img class="img-search" src="'.($avatar).'" style="width: 50px; height: 50px;"></div>
         <div id="user'.$row["id"].'" class="username-search" status="'.$row["usrstatus"].'"><span class="chatname'.$txt_unread.'">'.$row["display_name"].'</span> <span class="me" style="color: '.($isunread ? '#0084ff' : '#0006').'; margin-right: 17px;" title="'.$row['date'].'">'.$row['time'].'</span></div>
         <div class="last-message'.$txt_unread.'">'.($id == $row["last_sender_id"] ? 'You: ' : '').$msg.'</div>
             </div>';
