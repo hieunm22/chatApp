bbe584a21f3d726f2d43bab0257ca3e470ba9d63
Ming Hieu <hieunm1987@gmail.com>
2018-11-13 14:38:38 +0700

    last message in search list error

--- include/index_searchall.php
+++ include/index_searchall.php
@@ -21,7 +21,7 @@
 		echo '<div class="lbl search-result active-msg" draggable="true">
 		<div class="avatar-img"><img class="img-search" src="'.($avatar).'"></div>
         <div id="user'.$row["id"].'" class="username-search" status="'.$row["usrstatus"].'"><span class="chatname'.$txt_unread.'">'.$row["alias"].'</span> <span class="me" style="color: '.($isunread ? '#0084ff' : '#0006').';" title="'.$row['date'].'">'.$row['time'].'</span></div>
-        <div class="last-msg-row"><div class="last-message'.$txt_unread.'">'.($uid == $row["last_sender_id"] ? 'You: ' : '').$msg.'</div></div>
+        <div class="last-msg-row"><div class="last-message'.$txt_unread.'">'.($uid == $row["last_sender_id"] &&  $row['message_type'] == 0 ? 'You: ' : '').$msg.'</div></div>
             </div>';
 
         while ($row = mysqli_fetch_array($query)) {
@@ -52,7 +52,7 @@
             echo '<div class="lbl search-result" draggable="true">
             <div class="avatar-img"><img class="img-search" src="'.($avatar).'"></div>
             <div id="user'.$row["id"].'" class="username-search" status="'.$row["usrstatus"].'"><span class="chatname'.$txt_unread.'">'.$row["alias"].'</span> <span class="me" style="color: '.($isunread ? '#0084ff' : '#0006').';" title="'.$row['date'].'">'.$row['time'].'</span></div>
-            <div class="last-msg-row"><div class="last-message'.$txt_unread.'">'.($uid == $row["last_sender_id"] ? 'You: ' : '').$msg.'</div></div>
+            <div class="last-msg-row"><div class="last-message'.$txt_unread.'">'.($uid == $row["last_sender_id"] &&  $row['message_type'] == 0 ? 'You: ' : '').$msg.'</div></div>
                 </div>';
         }
 ?>
