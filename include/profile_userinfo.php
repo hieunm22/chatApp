079fc2c94f362e85dd308df84eb94d2ec2f14437
Ming Hieu <hieunm1987@gmail.com>
2018-12-28 00:18:49 +0700

    add change avatar function in profile page

--- include/profile_userinfo.php
+++ include/profile_userinfo.php
@@ -1,29 +1,30 @@
     <h2 class="header">Thông tin cá nhân</h2>
-    <table>
+    <table border="0">
         <tr>
-			<td class="avatar" rowspan="5"><img src="<?php echo $profile['avatar_url']?>" width="100px" height="100px" /></td>
-            <td class="lbl register">Tên đăng nhập</td>
+        <td class="avatar" rowspan="4"><img src="<?php echo $profile['avatar_url']?>" width="100px" height="100px" /></td>
+            <td class="register">Tên đăng nhập</td>
             <td class="userinfo"><strong><?php echo $profile['name']?></strong></td>
         </tr>
         <tr>
-            <td class="lbl register">Họ tên</td>
+            <td class="register">Họ tên</td>
             <td class="userinfo"><?php echo $profile['alias']?></td>
         </tr>
         <tr>
-            <td class="lbl register">Email</td>
+            <td class="register">Email</td>
             <td class="userinfo"><?php echo $profile['email']?></td>
         </tr>
         <tr>
-            <td class="lbl register">Số điện thoại</td>
+            <td class="register">Số điện thoại</td>
             <td class="userinfo"><?php echo $profile['phone']?></td>
         </tr>
 <?php
     if ($_GET['id'] == $_SESSION['user']['id']) {
         echo '        <tr>
-        <td class="lbl register" colspan="2">
-            <form method="post" action="profile.php" enctype="multipart/form-data">
-                <input type="file" name="avatar"/>
-                <input type="submit" id="change-avatar" name="uploadclick" value="Upload"/>
+        <td class="register"></td>
+        <td class="userinfo" colspan="3">
+            <form method="post" action="profile.php?id='.$_SESSION['user']['id'].'" enctype="multipart/form-data">
+                <span><input type="file" name="avatar"/></span>
+                <span><input type="submit" class="lbl" id="change-avatar" name="uploadclick" value="Upload"/></span>
             </form>
         </td>
     </tr>';
--- profile.php
+++ profile.php
@@ -16,26 +16,52 @@
             session_start();
             $con = initConnection();
             if (!isset($_SESSION['user'])) {
-                include('include/non_authorize.php');
+                include('include/non_authorize.html');
                 return;
             }
             include('include/welcome.php');
-            if (!isset($_GET['id'])) {
+            if ($_SERVER['REQUEST_METHOD'] == 'GET' && !isset($_GET['id'])) {
                 echo '
                 <script>(function redirect() { window.location.href="profile.php?id='.$_SESSION['user']['id'].'"; })();</script>';
+                return;
             }
-            $sql = "SELECT * FROM users where `id`='".$_GET['id']."' and status=1";
-            $query = mysqli_query($con, $sql);
-            $profile = mysqli_fetch_array($query);
-            echo '
-        <script id="temp">(function changeTitle() { $("title").text("'.$profile['alias'].'"); $("script#temp").remove(); })();</script>';
-            include('include/navigation.php');
-            if ($profile == null) {
-                echo '<div class="required"><strong>User không tồn tại.</strong></div>';
+            
+            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+                $sql = "SELECT * FROM users where `id`='".$_GET['id']."' and status=1";
+                $query = mysqli_query($con, $sql);
+                $profile = mysqli_fetch_array($query);
+                echo '
+            <script id="temp">(function changeTitle() { $("title").text("'.$profile['alias'].'"); $("script#temp").remove(); })();</script>';
+                include('include/navigation.php');
+                if ($profile == null) {
+                    echo '<div class="required"><strong>User không tồn tại.</strong></div>';
+                }
+                else {
+                    include('include/profile_userinfo.php');
+                }
             }
-            else {
-                include('include/profile_userinfo.php');
+            else {  // REQUEST_METHOD = 'POST'
+                $sql = "SELECT * FROM users where `id`=".$_SESSION['user']['id']." and status=1";
+                $query = mysqli_query($con, $sql);
+                $profile = mysqli_fetch_array($query);
+                echo '
+            <script id="temp">(function changeTitle() { $("title").text("'.$profile['alias'].'"); $("script#temp").remove(); })();</script>';
+                include('include/navigation.php');
+                // Nếu người dùng có chọn file để upload
+                
+                if ($_FILES['avatar']['error'] == 0)
+                {
+                    // Upload file
+                    move_uploaded_file($_FILES['avatar']['tmp_name'], './images/'.$_SESSION['user']['id'].'.jpg');
+                    include('include/profile_userinfo.php');
+                }
+                else if ($_FILES['avatar']['error'] == 4) 
+                {
+                    include('include/profile_userinfo.php');
+                    echo '<div class="login-message ios-shake-incorrect-passcode">Bạn chưa chọn file upload</div>';
+                }
             }
+
         ?>
 	</body>
 </html>
