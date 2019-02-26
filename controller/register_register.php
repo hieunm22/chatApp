a459af223b881c11279ad70142948595d3e1fdee
Ming Hieu <hieunm1987@gmail.com>
2019-02-26 17:55:23 +0700

    set ['current_connect'] on register successful

--- controller/register_register.php
+++ controller/register_register.php
@@ -1,83 +1,84 @@
 <?php
-    include('../default.php');
-    session_start();
+	include('../default.php');
+	session_start();
 
-    $usr = $_REQUEST['u'];
-    $usr = str_replace("'","\\'",$usr);
-    $eml = $_REQUEST['e'];
-    $eml = str_replace("'","\\'",$eml);
-    // $sql = "SELECT * FROM users where name='".$usr."' or (email='".$eml."' and '".$eml."'!='')'";
-    $sql = "SELECT * FROM users where name='".$usr."'";
-    if ($eml !== '') {
-        $sql .= " or email='".$eml."'";
-    }
-    $con = initConnection();
+	$usr = $_REQUEST['u'];
+	$usr = str_replace("'","\\'",$usr);
+	$eml = $_REQUEST['e'];
+	$eml = str_replace("'","\\'",$eml);
+	// $sql = "SELECT * FROM users where name='".$usr."' or (email='".$eml."' and '".$eml."'!='')'";
+	$sql = "SELECT * FROM users where name='".$usr."'";
+	if ($eml !== '') {
+		$sql .= " or email='".$eml."'";
+	}
+	$con = initConnection();
 	$query = mysqli_query($con, $sql);
 	$rowcount = mysqli_num_rows($query);
-    if ($rowcount == 0) {
-        $ali = $_REQUEST['a'];
-        $ali = str_replace("'","\\'",$ali);
-        $gen = $_REQUEST['g'];
-        $pho = $_REQUEST['f'];
-        $pho = str_replace("'","\\'",$pho);
-        $pwd = $_REQUEST['p'];
-        $pwd = str_replace("'","\\'",$pwd);
+  if ($rowcount == 0) {
+		$ali = $_REQUEST['a'];
+		$ali = str_replace("'","\\'",$ali);
+		$gen = $_REQUEST['g'];
+		$pho = $_REQUEST['f'];
+		$pho = str_replace("'","\\'",$pho);
+		$pwd = $_REQUEST['p'];
+		$pwd = str_replace("'","\\'",$pwd);
 
-        $sql = "Call insertUser('".$usr."', '".md5($pwd)."'";
-        if (!isset($eml) || trim($eml)==='') {
-            $sql .= ", null";
-        }
-        else {
-            $sql .= ", '".$eml."'";
-        }
-        if (!isset($ali) || trim($ali)==='') {
-            $sql .= ", null";
-        }
-        else {
-            $sql .= ", '".$ali."'";
-        }
+		$sql = "Call insertUser('".$usr."', '".md5($pwd)."'";
+		if (!isset($eml) || trim($eml)==='') {
+			$sql .= ", null";
+		}
+		else {
+			$sql .= ", '".$eml."'";
+		}
+		if (!isset($ali) || trim($ali)==='') {
+			$sql .= ", null";
+		}
+		else {
+			$sql .= ", '".$ali."'";
+		}
 		$sql .= ", ".$gen;
-        if (!isset($pho) || trim($pho)==='') {
-            $sql .= ", null";
-        }
-        else {
-            $sql .= ", '".$pho."'";
-        }
-        $sql .= ")";
-        $query = mysqli_query($con, $sql);
+		if (!isset($pho) || trim($pho)==='') {
+			$sql .= ", null";
+		}
+		else {
+			$sql .= ", '".$pho."'";
+		}
+		$sql .= ")";
+		$query = mysqli_query($con, $sql);
 
-        $sql = "select * from `users` where `name`='".$usr."'";
-        $query = mysqli_query($con, $sql);
+		$sql = "select * from `users` where `name`='".$usr."'";
+		$query = mysqli_query($con, $sql);
 		$row = mysqli_fetch_array($query);
-        $_SESSION['user'] = $row;
-        if (!file_exists('../users/'.$row["id"])) {
-            mkdir('../users/'.$row["id"], 0777, true);
-            $myfile = fopen("../users/".$row['id'].'/index.php', "w") or die("Unable to open file!");
-            $txt = "<html>
+		$_SESSION['user'] = $row;
+		if (!file_exists('../users/'.$row["id"])) {
+			mkdir('../users/'.$row["id"], 0777, true);
+			$myfile = fopen("../users/".$row['id'].'/index.php', "w") or die("Unable to open file!");
+			$txt = "<html>
     <head>
         <title>".$row['alias']."</title>
     </head>
     <body>
     </body>
 </html>";
-            fwrite($myfile, $txt);
-            fclose($myfile);
-        }
-        echo 0; // đăng ký thành cmn công
-    }
-    else {
-        $sql = "SELECT * FROM users where name='".$usr."'";
-        $query = mysqli_query($con, $sql);
-        $rowcount = mysqli_num_rows($query);
-        if ($rowcount == 1) {
-            echo 1; // trùng username
-            return;
-        }
-        $sql = "SELECT * FROM users where email='".$eml."'";
-        $query = mysqli_query($con, $sql);
-        $rowcount = mysqli_num_rows($query);
-        if ($rowcount == 1) {
-            echo 2; // trùng email
-        }
-    }
+			fwrite($myfile, $txt);
+			fclose($myfile);
+		}
+		$_SESSION['current_connect'] = -1;
+		echo 0; // đăng ký thành cmn công
+	}
+	else {
+		$sql = "SELECT * FROM users where name='".$usr."'";
+		$query = mysqli_query($con, $sql);
+		$rowcount = mysqli_num_rows($query);
+		if ($rowcount == 1) {
+			echo 1; // trùng username
+			return;
+		}
+		$sql = "SELECT * FROM users where email='".$eml."'";
+		$query = mysqli_query($con, $sql);
+		$rowcount = mysqli_num_rows($query);
+		if ($rowcount == 1) {
+			echo 2; // trùng email
+		}
+	}
 ?>
