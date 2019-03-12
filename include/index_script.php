592fb1da0a76629f12c33179633b6aeb4bc3beb6
Ming Hieu <hieunm1987@gmail.com>
2019-03-12 15:11:45 +0700

    login script events

--- include/index_script.php
+++ include/index_script.php
@@ -1,6 +1,6 @@
 <?php
 	if (!isset($_SESSION['user'])) {
-		// include('include/index_loginjs.html');
+		include('include/index_loginjs.html');
 	}
 	else {
 		$user = $_SESSION['user'] === null ? $_SESSION['user']['name'] : $_SESSION['user']['alias'];
